1c2c49a33557250fc8ab3e5fea0cbe04bd2aaf58
dehos <petangpuluhlikur@gmail.com>
2026-09-06 14:25:44 +0700

    Simplify database status update message

--- script.js
+++ script.js
@@ -858,16 +858,7 @@ async function loadBarang() {
     dataBarang =
         semuaBarang;
 
-   setDatabaseStatus(`
-    <svg width="18" height="18" viewBox="0 0 24 24" fill="none"
-         stroke="currentColor" stroke-width="2"
-         stroke-linecap="round" stroke-linejoin="round">
-        <ellipse cx="12" cy="5" rx="9" ry="3"></ellipse>
-        <path d="M3 5v14c0 1.66 4.03 3 9 3s9-1.34 9-3V5"></path>
-        <path d="M3 12c0 1.66 4.03 3 9 3s9-1.34 9-3"></path>
-    </svg>
-    ${formatNumber(dataBarang.length)}
-`);
+   setDatabaseStatus(`Database ${formatNumber(dataBarang.length)} barang`);
     updateTable();
 }
 
